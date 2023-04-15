5a67aa6cdc1e23f4392cdef177979c5cf92f109a
Daniel Emenike <danieloemenike@gmail.com>
2023-04-15 04:55:05 +0100

    url updated

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -43,7 +43,7 @@ const Customizer = () => {
         try
         {
             setGeneratingImg(true)
-            const response = await fetch('http://localhost:8080/api/v1/dalle', {
+            const response = await fetch('https://project-3d-clothing.onrender.com/api/v1/dalle', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
